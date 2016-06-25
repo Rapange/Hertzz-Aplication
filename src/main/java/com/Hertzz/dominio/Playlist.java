a8ecb7e97e479d2bd316d6805dbd35dac0785252
Rapange <h_ero@hotmail.es>
2016-06-24 21:50:14 -0500

    Playlist functions
    More functions

--- src/main/java/com/Hertzz/dominio/Playlist.java
+++ src/main/java/com/Hertzz/dominio/Playlist.java
@@ -20,23 +20,41 @@ public class Playlist{
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Playlist_ID_GENERATOR")	
 	private Integer playlist_id;
 	private Integer usuario_id;
-
 	@ManyToMany
 	@JoinTable(name = "Playlist_Cancion",
 		joinColumns = @JoinColumn(name = "playlist_ID", referencedColumnName = "playlist_id"),
 		inverseJoinColumns = @JoinColumn(name = "Cancion_ID", referencedColumnName = "ID"))
 	public List<Cancion> Canciones_playlist;
+	private String Nombre_playlist;
 
-	
-	public String Nombre_playlist;
-
-	public Playlist()
+	public Playlist(String nombre, Integer usuario_id)
 	{
+		this.Nombre_playlist = nombre;
+		this.usuario_id = usuario_id;
 	}
 	
 	
-	
-	
+	public Integer get_Usuario_Id(){
+		return this.usuario_id;
+	}
+	public String get_Nombre_playlist(){
+		return this.Nombre_playlist;
+	}
+	public List<Cancion> get_Canciones_playlist(){
+		return this.Canciones_playlist;
+	}
+	public boolean find_Cancion(Cancion cancion){
+		for(int i = 0; i < Canciones_playlist.size(); i++){
+			if(Canciones_playlist.get(i) == cancion){
+				return true;
+			}
+		}
+		return false;
+	}
+	public void add_Cancion(Cancion cancion){
+		Canciones_playlist.add(cancion);
+		return;
+	}
 	
 	public void Reproducir_aleatorio()
 	{
