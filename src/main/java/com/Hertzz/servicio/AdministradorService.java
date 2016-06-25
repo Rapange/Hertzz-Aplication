d43ec548e9ad8d66521b459a5ce11605842f8a91
Rapange <h_ero@hotmail.es>
2016-06-24 21:55:09 -0500

    Agregaralbum
    Agregar album

--- src/main/java/com/Hertzz/servicio/AdministradorService.java
+++ src/main/java/com/Hertzz/servicio/AdministradorService.java
@@ -1,7 +1,9 @@
 package com.Hertzz.servicio;
 
+import com.Hertzz.dominio.Album;
 import com.Hertzz.dominio.Artista;
 
 public interface AdministradorService {
-	boolean agregarArtista(Artista artista); 
+	boolean agregarArtista(Artista artista);
+	boolean agregarAlbum(Album album);
 }
