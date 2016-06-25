e9d0dc0478f8f9c26ac9464efa5027b4594b1b9a
Rapange <h_ero@hotmail.es>
2016-06-24 21:58:44 -0500

    Usuario Service

---
+++ src/main/java/com/Hertzz/servicio/UsuarioService.java
@@ -0,0 +1,11 @@
+package com.Hertzz.servicio;
+
+import com.Hertzz.dominio.Playlist;
+
+public interface UsuarioService {
+	boolean crear_playlist(String nombre, Integer usuario_id);
+	boolean crear_playlist(Playlist playlist, Integer usuario_id);
+	boolean agregar_cancion(Integer cancion_id, Integer playlist_id);
+	boolean seguir_usuario(Integer usuario_id, Integer usuario_id2);
+	boolean stop_seguir(Integer usuario_id, Integer usuario_id2);
+}
