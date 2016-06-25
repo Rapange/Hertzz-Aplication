52a0c32122e0e58c8f0d04d62c520e11941cedea
Rapange <h_ero@hotmail.es>
2016-06-25 00:06:30 -0500

    Update UsuarioRepositorio.java

--- src/main/java/com/Hertzz/repositorio/UsuarioRepositorio.java
+++ src/main/java/com/Hertzz/repositorio/UsuarioRepositorio.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.Hertzz.dominio.Cancion;
+import com.Hertzz.dominio.Playlist;
 import com.Hertzz.dominio.Usuario;
 
 public interface UsuarioRepositorio extends CrudRepository<Usuario, Integer> {
@@ -16,11 +17,18 @@ public interface UsuarioRepositorio extends CrudRepository<Usuario, Integer> {
 	//void Agregar_Playlist(Playlist playlist);
 	//void Agregar_Cancion(Integer cancion_id, Integer playlist_id);
 	//bool Crear_playlist(String nombre);
-	//bool Eliminar_playlist(Playlist);
+	@Query("DELETE FROM Playlist c WHERE c.playlist_id = :playlist_id")
+	boolean Eliminar_playlist(Integer playlist_id);
+	
+	@Query("DELETE FROM Usuario c WHERE c.Usuario_id = :usuario_id")
+	boolean Eliminarse(Integer usuario_id);
 	//void Copiar_playlist(Playlist lista);
 	//void Calificar_cancion(Integer numero);
 	//void Cerrar_cuenta();
 	//Cancion Buscar_Cancion(String nombre);
+	
+	@Query("SELECT c FROM Playlist AS c WHERE nombre_playlist = :nombre AND usuario_id = :usuario_id")
+	Playlist Buscar_Playlist(String nombre, Integer usuario_id);
 
 	@Query("SELECT c FROM Cancion AS c WHERE nombre_cancion = :nombre")
 	Cancion Buscar_Cancion(String nombre);
