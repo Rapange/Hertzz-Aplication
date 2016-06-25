b0478db09c3b9bfab8db3eac14248b4c389ae997
Rapange <h_ero@hotmail.es>
2016-06-24 23:50:46 -0500

    Cancion update

--- src/main/java/com/Hertzz/dominio/Cancion.java
+++ src/main/java/com/Hertzz/dominio/Cancion.java
@@ -18,13 +18,92 @@ public class Cancion {
 	private String Nombre_Cancion;
 	private String Fecha_Publicacion;
 	private String Letra_cancion;
-	private String Calificacion_Cancion;
+	private Integer Calificacion_Cancion;
 	private Integer Reproducciones;
 	private Integer Anteriror_Ranking_nota;
 	private Integer Anteriror_Ranking_popular;
 	
 	public Cancion(){
 	
+	}
+
+	public Cancion(Integer artista_id, Integer album_id, String Nombre_autor, String Nombre_cancion, String Letra_cancion){
+		this.Artista_id = artista_id;
+		this.Album_id = album_id;
+		this.Nombre_Autor = Nombre_autor;
+		this.Nombre_Cancion = Nombre_cancion;
+		this.Letra_cancion = Letra_cancion;
+	}
+	public Integer getCalificacion_Cancion() {
+		return Calificacion_Cancion;
+	}
+
+	public void setCalificacion_Cancion(Integer calificacion_Cancion) {
+		Calificacion_Cancion = calificacion_Cancion;
+	}
+
+	public Integer getArtista_id() {
+		return Artista_id;
+	}
+
+	public void setArtista_id(Integer artista_id) {
+		Artista_id = artista_id;
+	}
+
+	public Integer getAlbum_id() {
+		return Album_id;
+	}
+
+	public void setAlbum_id(Integer album_id) {
+		Album_id = album_id;
+	}
+
+	public String getNombre_Autor() {
+		return Nombre_Autor;
+	}
+
+	public void setNombre_Autor(String nombre_Autor) {
+		Nombre_Autor = nombre_Autor;
+	}
+
+	public String getNombre_Cancion() {
+		return Nombre_Cancion;
+	}
+
+	public void setNombre_Cancion(String nombre_Cancion) {
+		Nombre_Cancion = nombre_Cancion;
+	}
+
+	public String getLetra_cancion() {
+		return Letra_cancion;
+	}
+
+	public void setLetra_cancion(String letra_cancion) {
+		Letra_cancion = letra_cancion;
+	}
+
+	public Integer getReproducciones() {
+		return Reproducciones;
+	}
+
+	public void setReproducciones(Integer reproducciones) {
+		Reproducciones = reproducciones;
+	}
+
+	public Integer getAnteriror_Ranking_nota() {
+		return Anteriror_Ranking_nota;
+	}
+
+	public void setAnteriror_Ranking_nota(Integer anteriror_Ranking_nota) {
+		Anteriror_Ranking_nota = anteriror_Ranking_nota;
+	}
+
+	public Integer getAnteriror_Ranking_popular() {
+		return Anteriror_Ranking_popular;
+	}
+
+	public void setAnteriror_Ranking_popular(Integer anteriror_Ranking_popular) {
+		Anteriror_Ranking_popular = anteriror_Ranking_popular;
 	}
 	
 	
